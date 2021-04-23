c4db2549829ca082c1ec0e8d373b8ee596341a85
Bloomkd46 <Bloomkd46@icloud.com>
2021-04-22 19:34:45 -0700

    Worked on KoltonsFirstTankDrive Program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KoltonsFirstTankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KoltonsFirstTankDrive.java
@@ -12,23 +12,25 @@ import org.firstinspires.ftc.robotcore.external.navigation.Rotation;
 @TeleOp(name = "KoltonsFirstTankDrive", group = "ForRobot")
 
 public class KoltonsFirstTankDrive extends LinearOpMode {
-    private DcMotor Motor1;
-    private DcMotor Motor2;
+    private DcMotorEx Motor1 = hardwareMap.get(DcMotorEx.class, "Motor_0");
+    private DcMotorEx Motor2 = hardwareMap.get(DcMotorEx.class, "Motor_1");
+    private DcMotorEx Motor3 = hardwareMap.get(DcMotorEx.class, "Motor_2");
+    private DcMotorEx Motor4 = hardwareMap.get(DcMotorEx.class, "Motor_3");
+    double MotorTicks = 28;
 
     @Override
     public void runOpMode() {
-        Motor1 = hardwareMap.get(DcMotor.class, "Motor_0");
-        Motor2 = hardwareMap.get(DcMotor.class, "Motor_1");
         Motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         Motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        double MotorTicks = 28;
         waitForStart();
         if (opModeIsActive()) {
             while (opModeIsActive()) {
-                double MotorLeft = Math.pow(-gamepad1.left_stick_y*MotorTicks, 3);
-                double MotorRight = Math.pow(-gamepad1.right_stick_y*MotorTicks, 3);
-                ((DcMotorEx) Motor1).setVelocity(MotorLeft);
-                ((DcMotorEx) Motor2).setVelocity(MotorRight);
+                double MotorLeft = Math.pow(-gamepad1.left_stick_y, 3);
+                double MotorRight = Math.pow(-gamepad1.right_stick_y, 3);
+                Motor1.setPower(MotorLeft);
+                Motor2.setPower(MotorRight);
+                Motor3.setVelocity();
+                Motor4.setVelocity();
             }
         }
     }
