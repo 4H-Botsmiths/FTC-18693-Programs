5659ce384cfa5eed5b0ac6cb2056319cf6d5d66b
bloomkd46 <75853497+bloomkd46@users.noreply.github.com>
2021-08-22 13:27:32 -0700

    #Update This Is the first recorded update.

--- update-changelogs.sh
+++ update-changelogs.sh
@@ -5,7 +5,9 @@ git log --stat --author "bloomkd46" >>Koltons-Changelog.txt
 echo to update run ./update-changlelogs.sh >Williams-Changelog.txt
 git log --stat --author "StrongMan19" >>Williams-Changelog.txt
 
-git add Koltons-Changelog.txt Williams-Changelog.txt
-git commit -m "Updated Koltons-Changelog.txt and Williams-Changelog.txt" >/dev/null
+git log git log -S"#Update" >Project-Changelog.txt
+
+git add Koltons-Changelog.txt Williams-Changelog.txt Project-Changelog.txt
+git commit -m "Updated Koltons-Changelog.txt, Williams-Changelog.txt, and Project-Changelog.txt" >/dev/null
 
 echo Changelogs Successfully Updated
