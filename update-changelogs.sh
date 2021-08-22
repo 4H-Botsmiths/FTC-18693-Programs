23ed5d1dd9aa96b01070184fed9b552ac2fb98c9
bloomkd46 <75853497+bloomkd46@users.noreply.github.com>
2021-08-22 14:25:11 -0700

    Updated update-changelogs.sh

--- update-changelogs.sh
+++ update-changelogs.sh
@@ -1,15 +1,17 @@
 #/bin/bash/sh
 echo to update run ./update-changlelogs.sh >Koltons-Changelog.txt
-git log --stat --author="bloomkd46" --grep="Updated Koltons-Changelog.txt, Williams-Changelog.txt, and Project-Changelog.txt" --invert-grep >>Koltons-Changelog.txt
+git log --author="bloomkd46" --stat >>Koltons-Changelog.txt
+#--grep="Updated Koltons-Changelog.txt, Williams-Changelog.txt, and Project-Changelog.txt" --invert-grep
 
 echo to update run ./update-changlelogs.sh >Williams-Changelog.txt
-git log --stat --author="StrongMan19" --grep="Updated Koltons-Changelog.txt, Williams-Changelog.txt, and Project-Changelog.txt" --invert-grep >>Williams-Changelog.txt
+git log --author="StrongMan19" --stat >>Williams-Changelog.txt
+#--grep="Updated Koltons-Changelog.txt, Williams-Changelog.txt, and Project-Changelog.txt" --invert-grep
 
 echo to update run ./update-changlelogs.sh, to make commits apear add Update: to the start of them >Project-Changelog.txt
 git log --grep="Update:" >>Project-Changelog.txt
 
 git add Koltons-Changelog.txt Williams-Changelog.txt Project-Changelog.txt
 git commit -m "Updated Koltons-Changelog.txt, Williams-Changelog.txt, and Project-Changelog.txt" >/dev/null
-git push >/dev/null
+
 
 echo Changelogs Successfully Updated
