a5d011c6bc7e85a290a590984a78f638f12d6553
bloomkd46 <75853497+bloomkd46@users.noreply.github.com>
2021-08-22 14:07:57 -0700

    Updated update-changelogs.sh

--- update-changelogs.sh
+++ update-changelogs.sh
@@ -1,9 +1,9 @@
 #/bin/bash/sh
 echo to update run ./update-changlelogs.sh >Koltons-Changelog.txt
-git log --stat --author "bloomkd46" --grep="Updated Koltons-Changelog.txt, Williams-Changelog.txt, and Project-Changelog.txt" --invert-grep >>Koltons-Changelog.txt
+git log --stat --author="bloomkd46" --grep="Updated Koltons-Changelog.txt, Williams-Changelog.txt, and Project-Changelog.txt" --invert-grep >>Koltons-Changelog.txt
 
 echo to update run ./update-changlelogs.sh >Williams-Changelog.txt
-git log --stat --author "StrongMan19" --grep="Updated Koltons-Changelog.txt, Williams-Changelog.txt, and Project-Changelog.txt" --invert-grep >>Williams-Changelog.txt
+git log --stat --author="StrongMan19" --grep="Updated Koltons-Changelog.txt, Williams-Changelog.txt, and Project-Changelog.txt" --invert-grep >>Williams-Changelog.txt
 
 echo to update run ./update-changlelogs.sh, to make commits apear add Update: to the start of them >Project-Changelog.txt
 git log --grep="Update:" >>Project-Changelog.txt
